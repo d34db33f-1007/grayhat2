291bfb82b6af66d22fcbc334e9aede96a265721c
d34db33f-1007 <63560479+d34db33f-1007@users.noreply.github.com>
2020-08-03 23:48:01 +0300

    Add files via upload

---
+++ grayhat.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import sys, re
+import requests, json
+
+from typing import List
+
+
+
+class ForbiddenAccess(Exception):
+	pass
+
+
+class Build:
+
+	def Bucket(api_k: str, b_id: str, start = 0, stop=None, ext: List[str] = []):
+		buck = 'https://buckets.grayhatwarfare.com/api/v1'
+		buck += f'/bucket/{b_id}/files'
+		buck += f'/{start}/{stop}' if stop else f'/{start}'
+		buck += f'?access_token={api_k}'
+		if ext and 0 < len(ext) < 5:
+			with open('exclude.txt', 'r') as negative:
+				for trash in negative.read().split(','):
+					if trash.startswith('-'):
+						ext += trash.split()
+		if len(ext) > 0:
+			buck += f'&keywords='
+			e = '%20' if len(ext) > 1 else ''
+			for ex in ext:
+				n = ext.index(ex)
+				if n > 0 and not ex.startswith('-'):
+					continue
+				buck += f'{ex}{e}' if n < (len(ext) -1) else f'{ex}'
+		return buck
+
+
+class s3:
+
+	def __init__(self, url: str):
+		self.pl = url
+		self.list = []
+
+	def search(self):
+		try:
+			resp = requests.get(self.pl)
+		except:
+			return False
+		if resp.status_code == 200:
+			return resp.json()
+		elif resp.status_code == 401:
+			raise ForbiddenAccess('Check your access token.')
+		else:
+#			print(f'[Code: {resp.status_code}] Connection error..')
+			return False
+
+	def files(self, size: float = None, ext: List[str] = []):
+		Files = self.search()
+		if not Files:
+			return False
+		self.list.append(f"{Files['results']}")
+		for file in Files['files']:
+			buc = {}
+			buc['fid'] = file['id']
+			buc['bid'] = file['bucketId']
+			buc['bucket'] = file['bucket']
+			buc['filename'] = file['filename']
+			buc['file_path'] = file['fullPath']
+			buc['file_url'] = file['url']
+			mb = float(f"{int(file['size']) / 1048576}")
+			buc['file_size'] = f'{int(mb)} Mb'
+			if size:
+				if mb < float(size):
+					self.trash(file['url'])
+					continue
+			if re.split(r'[.-/-_\s]\s*', file['filename'])[-1] != ext[0]:
+				self.trash(file['url'])
+				continue
+			for x in ext[1:]:
+				if x.startswith('-'):
+					if x in re.split(r'[.-/-_\s]\s*', file['fullPath']):
+						if buc:
+							self.trash(file['url'])
+							del buc
+			try:
+				self.list.append(buc)
+			except:
+				pass
+		print(f"Bucket ID: {self.pl.split('bucket/')[1].split('/')[0]} / 94000", end="\r")
+		return self.list
+
+	def trash(self, url):
+		with open('trash.txt', 'a') as out:
+			out.write(f'{url}\n')
+
---
+++ main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import asyncio
+from grayhat import s3
+from grayhat import Build as bl
+#from concurrent.futures import ThreadPoolExecutor
+
+class Brute:
+
+	def __init__(self):
+		self.token = input('Enter your grayhatwarfare.com api token: ')
+		self.in1 = input('Which file extension to search? ex. zip: ')
+		if self.in1.startswith('.'):
+			self.in1 = self.in1.replace('.', '')
+		print('\nEnter minimum size of files in Mb: ex. 4000, 0.05')
+		self.in2 = input('Enter 0 if none: ')
+		self.extensions = self.in1.split()
+		self.keywords = self.extensions
+		with open('exclude.txt', 'r') as kw:
+			for w in kw.read().split(','):
+				if w.startswith('-'):
+					self.keywords += w.split()
+#		self.executor = ThreadPoolExecutor(30)
+
+	async def proc(self, bid, st: int = 0):
+		for __ in range(100000 // 998):
+			bless = bl.Bucket(api_k = self.token, b_id=bid, start=st, stop=998, ext=self.extensions)
+			warfare = s3(bless)
+			loop = asyncio.get_event_loop()
+			run = loop.run_in_executor(None, warfare.files, float(self.in2), self.keywords) # L24
+			res = await run
+			if not res:
+				break
+			elif res and len(res) < 2:
+				break
+			elif st > int(res[0]) or int(res[0]) == 0:
+				break
+			else:
+				st += 998
+				with open('results.txt', 'a') as out:
+					for f in res[1:]:
+						txt = f"{f['bid']}  {f['file_url']}  {f['file_size']}\n"
+						out.write(txt)
+		if int(bid) % 100 == 0:
+			await asyncio.sleep(2)
+
+	async def hack(self):
+		await asyncio.gather(*[self.proc(bid=Id) for Id in range(1, 94000)])
+
+
+if __name__ == '__main__':
+	blazing = Brute()
+	asyncio.run(blazing.hack())
+
